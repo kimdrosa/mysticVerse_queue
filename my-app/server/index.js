39331192bda7c3f7d253d89cd3a43976fc513663
kimdrosa <kimdeeerosa@gmail.com>
2020-08-25 15:26:28 -0700

    fixed socket close event so server wouldnt crash when one socket closes, also added room to sort by uuid

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -13,45 +13,54 @@ app.get('/', function(req, res) {
 
 var server = new WebSocket.Server({server : app.listen(3333)});
 
-// app.use(express.static('public'));
 
 console.log('socket server is running on port 3333');
 
 const users = [];
-const maxTime =  60000;
+const maxTime =  30000;
 const userList = [];
 const activeUserList = [];
 const activeUsers = [{videoUrl : "https://en.wikipedia.org/wiki/Leprus_intermedius",
                           user : null,
                           connection : null,
+                          uuid : null,
                           time : null},
                         {videoUrl : "https://en.wikipedia.org/wiki/Bishkek-1_railway_station",
                          user : null,
                          connection : null,
+                         uuid : null,
                          time : null},
                         {videoUrl : "https://en.wikipedia.org/wiki/Schefflera_pueckleri",
                          user : null,
+                         uuid : null,
                          connection : null,
                          time : null}];
 
 
 server.on('connection', (socket) => {
-    
+ 
     socket.on('message', message => {
+  
+        let userInfo = JSON.parse(message);
+        
+        socket.id = userInfo.uuid;
+        console.log('socekt id : ' + socket.id);
       users.push({
-                  user: message,
+                  uuid : userInfo.uuid,
+                  user: userInfo.userName,
                   connection : socket
-                })
-      userList.push(message);
+                });
+      userList.push(userInfo.userName);
     });
+
     setInterval(() => {  
-        console.log(userList);
                 for(var i = 0; i < activeUsers.length; i++) {
                     if(activeUsers[i].user === null) {
                         if(users.length > 0) {
                         activeUsers[i].user = users[0].user;
                         activeUserList.push(userList[0]);
                         activeUsers[i].connection = users[0].connection;
+                        activeUsers[i].uuid = users[0].uuid;
                         users.shift();
                         userList.shift();
                         activeUsers[i].time = new Date();
@@ -60,15 +69,30 @@ server.on('connection', (socket) => {
                     } else if (checkTimeOut(activeUsers[i])) {
                         activeUsers[i].user = null;
                         activeUserList.shift();
+                        if(socket.readyState != 3) {
                         activeUsers[i].connection.send("Times Up!")
+                        }
                     }
                 }
-            
+                if(socket.readyState != 3) {
                 socket.send('{ "users":' + JSON.stringify(userList) + ',"activeUsers":' + JSON.stringify(activeUserList) + '}');
-             
+                } else {
+                    for(var k = 0; k < users.length; k++){
+                        if(users[k].uuid === socket.id) {
+                            users.splice(k,1);
+                        }
+                    }
+                    for(var j = 0; j < activeUsers.length; j++) {
+                        if(activeUsers[j].uuid === socket.id){
+                            users.splice(j, 1)
+                        }
+                    }
+                }
+                
                 
             }, 1000)
-
+            
+            
             function checkTimeOut (user) {
                         const currentTime = new Date()
                         if(currentTime - user.time >= maxTime) {
@@ -76,7 +100,7 @@ server.on('connection', (socket) => {
                         } else {
                             return false;
                         }}
-  });
+            });
 
 
 
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,6 +18,7 @@ class App extends React.Component {
         super(props);
         this.joinQueue = this.joinQueue.bind(this);
         this.leaveQueue = this.leaveQueue.bind(this);
+        this.openWebSocket = this.openWebSocket.bind(this);
         this.state = {
             user : null,
             videoURL : null,
@@ -33,48 +34,93 @@ class App extends React.Component {
    
 
     componentDidMount(){
-        socket.onopen = () => {
-            console.log('connection open')
-            }
+      // this.openWebSocket();
+        // socket.onopen = () => {
+        //     console.log('connection open')
+        //     }
   
-            socket.onmessage = event => {
-              console.log(`Message from server: ${event.data}`);
-              //if the message from the server starts with u then it has the user lists in it
-              if(event.data[3] === 'u'){
-              const allUsers = JSON.parse(event.data)
-              console.log('all users: ' + allUsers)
-              this.setState({
-                  users : allUsers,
-                  activeUsers : allUsers.activeUsers,
-                  queueUsers : allUsers.users
-                  })
-              //if it starts with v then it is the URL for the video endpoint
-              } else if (event.data[2] === 'v') {
-                const url = JSON.parse(event.data)
-                this.setState({
-                  showEnter: true,
-                  videoURL: url.videoUrl
-                  }) 
-              //if it starts with T then it is telling the user that time is up
-              } else if(event.data[2] === 'T') {
-                this.setState({
-                  showTimesUp: true
-                  }) 
-                }
-            }
+        //     socket.onmessage = event => {
+        //       console.log(`Message from server: ${event.data}`);
+        //       //if the message from the server starts with u then it has the user lists in it
+        //       if(event.data[3] === 'u'){
+        //       const allUsers = JSON.parse(event.data)
+        //       console.log('all users: ' + allUsers)
+        //       this.setState({
+        //           users : allUsers,
+        //           activeUsers : allUsers.activeUsers,
+        //           queueUsers : allUsers.users
+        //           })
+        //       //if it starts with v then it is the URL for the video endpoint
+        //       } else if (event.data[2] === 'v') {
+        //         const url = JSON.parse(event.data)
+        //         this.setState({
+        //           showEnter: true,
+        //           videoURL: url.videoUrl
+        //           }) 
+        //       //if it starts with T then it is telling the user that time is up
+        //       } else if(event.data[2] === 'T') {
+        //         this.setState({
+        //           showTimesUp: true
+        //           }) 
+        //         }
+        //     }
       };
     
+    openWebSocket() {
+      socket.onopen = () => {
+        console.log('connection open')
+        }
+
+        socket.onmessage = event => {
+          console.log(`Message from server: ${event.data}`);
+          //if the message from the server starts with u then it has the user lists in it
+          if(event.data[3] === 'u'){
+          const allUsers = JSON.parse(event.data)
+          console.log('all users: ' + allUsers)
+          this.setState({
+              users : allUsers,
+              activeUsers : allUsers.activeUsers,
+              queueUsers : allUsers.users
+              })
+          //if it starts with v then it is the URL for the video endpoint
+          } else if (event.data[2] === 'v') {
+            const url = JSON.parse(event.data)
+            this.setState({
+              showEnter: true,
+              videoURL: url.videoUrl
+              }) 
+          //if it starts with T then it is telling the user that time is up
+          } else if(event.data[2] === 'T') {
+            this.setState({
+              showTimesUp: true
+              }) 
+            }
+        }
+        // socket.onclose = () => {
+        //   this.openWebSocket()
+        // }
+    }
     //when the user decides to join the 3d verse (puts you in the world if it is not full and the queue if it is full)
     joinQueue() {
-      socket.send('playaName');
+      this.openWebSocket();
+      let randInt = Math.random() * Math.floor(1000);
+      let userId = randInt;
+      let username = 'playaName';
+      socket.send('{"uuid":' + JSON.stringify(userId) +',' +
+                    '"userName":' + JSON.stringify(username) + '}');
       this.setState({
         isInQueue: true, 
-        user : 'playaName'
-      })
-    }
+        user : 'playaName' + randInt.toString()
+      
+    })
+  }
 
     //when a user decides to leave the queue
     leaveQueue(){
+      socket.close();
+      this.setState({
+        isInQueue: false
+      })
 
     }
 
@@ -102,7 +148,7 @@ class App extends React.Component {
                 </div>
                 <div>
       <p><b>Number of people waiting: {this.state.queueUsers.length.toString()}</b></p>
-              <Button onClick={this.joinQueue}>Join</Button>
+              <Button onClick={this.leaveQueue}>Leave</Button>
               </div>
               </Card.Body>
             </Accordion.Collapse>
@@ -130,32 +176,6 @@ class App extends React.Component {
           </Modal.Footer>
         </Modal.Dialog>
       )
-    } else if(!this.state.showEnter && !this.state.showTimesUp && this.state.activeUsers != null && this.state.queueUsers != null && this.state.isInQueue) {
-      console.log(this.state.activeUsers)
-    return (
-      <Accordion>
-        <Card>
-        
-          <Accordion.Toggle id="queueToggle" as={Button} variant='primary' eventKey="1">
-              Enter The 3D Verse
-             
-          </Accordion.Toggle>
-        
-          <Accordion.Collapse eventKey="1">
-            <Card.Body> 
-              <div id='userLists'>
-                <ListOfActiveUsers users={this.state.activeUsers}/>
-                <ListOfQueueUsers users={this.state.queueUsers}/>
-              </div>
-              <div>
-    <p><b>Number of people waiting: {this.state.queueUsers.length.toString()}</b></p>
-            <Button onClick={this.leaveQueue}>Leave</Button>
-            </div>
-            </Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      </Accordion>
-);
 }else {
       return (
         <Accordion>
@@ -172,7 +192,7 @@ class App extends React.Component {
               
 
                 </div>
-              <Button onClick={this.joinQueue}>Join Queue</Button>
+              <Button onClick={this.joinQueue}>Join</Button>
 
               </Card.Body>
             </Accordion.Collapse>
